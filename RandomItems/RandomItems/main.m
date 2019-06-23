1bbb3301b6a71ed74eb183e0077467bf85144ea4
Austin <austin@tag219877.lan1>
2019-06-22 21:12:20 -0500

    Initial Commit

---
+++ RandomItems/RandomItems/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  RandomItems
+//
+//  Created by Austin on 6/22/19.
+//  Copyright © 2019 Austin. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
